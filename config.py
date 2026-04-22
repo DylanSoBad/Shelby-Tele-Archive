9acf943256ce352ddbcd8a840d1358d344032d6c
DylanBug <sadmen08@gmail.com>
2026-04-22 16:50:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
     
-    # Shelby configuration
+   
     SHELBY_API_KEY = os.getenv("SHELBY_API_KEY")
     SHELBY_ACCOUNT_ADDRESS = os.getenv("SHELBY_ACCOUNT_ADDRESS")
     SHELBY_ACCOUNT_PRIVATE_KEY = os.getenv("SHELBY_ACCOUNT_PRIVATE_KEY")
